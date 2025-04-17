a054a78ab3e4a37c878e4960f491381899253464
Sebastian Czech <sebaczech@gmail.com>
2025-04-18 00:45:36 +0200

    Update example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,13 +1,29 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/resource_group
 data "azurerm_resource_group" "this" {
   name = var.resource_group_name
+}
+
+# https://registry.terraform.io/providers/hashicorp/http/latest/docs/data-sources/http
+data "http" "this" {
+  url = "https://api.ipify.org" # "https://ifconfig.me"
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "this" {
+  length  = 16
+  special = false
 }
 
 module "storage_account" {
   source = "../../"
 
-  storage_account_name = "${substr(replace(data.azurerm_resource_group.this.name, "-", ""), 0, 21)}sa"
-  resource_group_name  = data.azurerm_resource_group.this.name
-  location             = data.azurerm_resource_group.this.location
+  # names are generated from the resource group name and abbreviation from https://learn.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-abbreviations
+  storage_account_name = "${substr(replace(data.azurerm_resource_group.this.name, "-", ""), 0, 21)}st"
+  key_vault_name       = "${random_string.this.result}-kv"
+  allowed_ip           = "${data.http.this.response_body}/32"
+
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
 
   tags = var.tags
 }
--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -6,6 +6,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=4.26.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.7.1"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "=3.4.5"
+    }
   }
 }
 
